cd081f55863f0ce0d73cb792b5a602b88a209359
Fikayomi31 <fikayoogundijo@gmail.com>
2024-01-30 09:23:33 -0800

    signup and login page

--- frontend/index.js
+++ frontend/index.js
@@ -4,6 +4,21 @@ const imageContainer = document.querySelector(".main--hero");
     imageContainer.style.backgroundImage = "url('./assets/dbdfc26503d1b6150202f1563142a02a.jpg')";
     imageContainer.style.backgroundSize = "100%";
     
-const imageKnow = document.querySelector(".main--container--knowus--image");
-    imageKnow.style.backgroundImage = "url"('./assets/landscape 2.png')
-    imageKnow.style.backgroundSize = "100%"
+function validateSignup () {
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+    const confirmPassword = document.getElementById('confirmPassword').value;
+
+    if (password !== confirmPassword) {
+        alert("Passwords do not match");
+        return;
+    }
+
+    // Perform additional validation if needed
+
+    // If all validation passes, you can submit the form or perform other actions
+    alert("Signup successful!");
+     // Redirect to the desired page (replace 'dashboard.html' with the actual page URL)
+     window.location.href = 'dashboard.html';
+
+}
